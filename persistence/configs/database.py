7c7d1d661ea3bcbedd571d29ceb0198290f0d0f4
pedrokkjzk <pirriquecsousa@gmail.com>
2026-05-29 16:43:38 -0300

    feat: add get_db_connection in database.py

--- persistence/configs/database.py
+++ persistence/configs/database.py
@@ -2,17 +2,26 @@ import os
 import asyncpg
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 pool: asyncpg.Pool = None
-
-async def get_pool() -> asyncpg.Pool:
-    return pool
 
 async def init_db():
     global pool
     pool = await asyncpg.create_pool(DATABASE_URL)
 
+
 async def close_pool():
     global pool
     if pool:
         await pool.close()
+        pool = None
+
+
+async def get_db_connection():
+    if pool is None:
+        raise RuntimeError("O pool de conexões não foi inicializado. Execute `init_db()` primeiro.")
+
+    conn = await pool.acquire()
+    try:
+        yield conn
+    finally:
+        await pool.release(conn)
