28cee7b0c79e7f76dea9ac30dc70388c63710767
pedrokkjzk <pirriquecsousa@gmail.com>
2026-05-30 20:36:47 -0300

    feat: create dtos to use in use_cases and router

---
+++ business/dtos/team_dto.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class CreateTeamDTO(BaseModel):
+    team_name: str
+    state: str
+    creation: date
+    manager_id: int
+    estadio_id: int
+
+
+class UpdateTeamDTO(BaseModel):
+    team_name: str | None = None
+    state: str | None = None
+    creation: date | None = None
+    manager_id: int | None = None
+    estadio_id: int | None = None
